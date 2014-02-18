cef6c204054ec44c2ba218e831daaefbf49927b4
marielson <marielson@marielson-HuronRiver-Platform>
2014-02-18 16:49:33 -0300

    Criação de caixa de diálogos personalizada

---
+++ FinancasPessoais/src/app/controllers/DialogoController.java
@@ -0,0 +1,19 @@
+package app.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+public class DialogoController {
+	@FXML
+	private Label mensasgem;
+	@FXML
+	private Button btOk;
+	
+	@FXML
+	public void initialize (){
+		
+	}
+	
+}
