d00b801c4bb79f97f8aab8e04df931640962f255
marielson <marielson@marielson-HuronRiver-Platform>
2014-03-12 17:49:00 -0300

    Composição Conta - Histórico

--- FinancasPessoais/src/model/Conta.java
+++ FinancasPessoais/src/model/Conta.java
@@ -1,6 +1,9 @@
 package model;
 
+import java.util.Date;
+
 public class Conta {
+	private Historico historico;
 	private Usuario pessoa;
 	private String agencia;
 	private String tipo;
@@ -10,57 +13,54 @@ public class Conta {
 	
 	public Conta(Usuario pessoa ,String banco, String agencia, String conta, String tipo) {
 		setPessoa(pessoa);
-		this.setBanco(banco);
-		this.setAgencia(agencia);
-		this.setConta(conta);
-		this.setTipo(tipo);
+		setBanco(banco);
+		setAgencia(agencia);
+		setConta(conta);
+		setTipo(tipo);
+		setHistorico(new Historico("Nova Conta", new Date(), "Nova conta criada"));
 	}
 
 	public String getAgencia() {
 		return agencia;
 	}
-
 	public void setAgencia(String agencia) {
 		this.agencia = agencia;
 	}
-
 	public String getTipo() {
 		return tipo;
 	}
-
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-
 	public String getConta() {
 		return conta;
 	}
-
 	public void setConta(String conta) {
 		this.conta = conta;
 	}
-
 	public String getBanco() {
 		return banco;
 	}
-
 	public void setBanco(String banco) {
 		this.banco = banco;
 	}
-
 	public double getSaldo() {
 		return saldo;
 	}
-
 	public void depositar(double saldo) {
 		this.saldo += saldo;
 	}
-
 	public Usuario getPessoa() {
 		return pessoa;
 	}
-
 	public void setPessoa(Usuario pessoa) {
 		this.pessoa = pessoa;
 	}
+	public Historico getHistorico() {
+		return historico;
+	}
+	public void setHistorico(Historico historico) {
+		this.historico = historico;
+	}
+	
 }
